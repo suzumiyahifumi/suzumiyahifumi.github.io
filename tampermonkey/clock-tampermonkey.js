75b2ceefd3a4a2e877fca28b5c2ca7ae2a44dd33
Liquid Sausage 100% <37862699+suzumiyahifumi@users.noreply.github.com>
2023-06-16 13:49:13 +0800

    clock-tampermonkey

---
+++ tampermonkey/clock-tampermonkey.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         clock-tampermonkey by suzumiyahifumi
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  上班打卡
+// @author       suzumiyahifumi
+// @match        https://clock.scsiis-island.com/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=scsiis-island.com
+// @grant        none
+// ==/UserScript==
+
+(function () {
+	'use strict';
+
+	// center 24.966133203581, 121.18768050493
+	var myCenter = [24.966133203581, 121.18768050493];
+	navigator.geolocation._getCurrentPosition = navigator.geolocation.getCurrentPosition;
+	navigator.geolocation.getCurrentPosition = function (success, error, options) {
+		return navigator.geolocation._getCurrentPosition(function (_position) {
+			let position = {
+				coords: {
+					latitude: myCenter[0],
+					longitude: myCenter[1]
+				}
+			};
+			success(position);
+		}, error, options);
+	};
+})();
